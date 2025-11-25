ad38a3703818703a468c27d346667ab83dec8c5c
Melissa <melissahuerta.dev@gmail.com>
2025-11-25 00:31:54 -0500

    script y escena del punto

--- waves-game/scripts/main.gd
+++ waves-game/scripts/main.gd
@@ -1,7 +1,30 @@
-extends Node
+extends Node2D
 
 # TODO>
 # creación aleatoria de puntos
 # creación de la onda
 # reinicios
 # colisiones y resultados
+
+@onready var point_scene = preload("res://scenes/Point.tscn")
+
+var start_point
+var target_point
+var goal_point
+
+func _ready() -> void:
+	spawn_points()
+	# TODO: esperar 0.3 , mostrar onda
+	
+func spawn_points():
+	# Crear punto de inicio de forma aleatoria
+	start_point = point_scene.instantiate()
+	start_point.point_type = "start"
+	start_point.position = get_random_position()
+	add_child(start_point)
+
+func get_random_position():
+	return Vector2(
+		randi_range(100, 1180),
+		randi_range(100, 620)
+	)
--- waves-game/scripts/point.gd
+++ waves-game/scripts/point.gd
@@ -1,1 +1,18 @@
-extends Node
+extends Node2D
+
+# Prefab para utilizar:
+# > punto de inicio (verde) - start
+# > punto destino invisible (amarillo) - target
+# > punto meta (morado) - goal
+
+@export var point_type: String = "start" # start | target | goal
+
+func _ready():
+	var mat : ShaderMaterial = $Circle.material
+	if point_type == "start":
+		mat.set("shader_parameter/color", Color("#A7E399")) # verde neon
+	elif point_type == "target":
+		mat.set("shader_parameter/color", Color("#F6FF99")) # amarillo
+		$Circle.visible = false
+	elif point_type == "goal":
+		mat.set("shader_parameter/color", Color("#B771E5")) # morado
